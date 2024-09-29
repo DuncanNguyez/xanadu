d0cdd3debcce89b1a6645a3d36aa7324196c014a
Duncan Nguyen <duncan.nguyen.18@gmail.com>
2024-09-30 01:23:15 +0700

    Setup: initialized identify module

---
+++ identify/src/main/java/identify/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package identify.config;
+
+import java.util.Collections;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+@Configuration
+@Slf4j
+public class CorsConfig {
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    CorsConfiguration config = new CorsConfiguration();
+    config.setAllowedOrigins(Collections.singletonList("*"));
+    config.setAllowedMethods(Collections.singletonList("*"));
+    config.setAllowedHeaders(Collections.singletonList("*"));
+    source.registerCorsConfiguration("/**", config);
+    return source;
+  }
+}
